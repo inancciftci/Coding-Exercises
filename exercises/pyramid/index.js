280f6d851ce12565ac63dcb146d7e807f9008d60
inancciftci <enonch@hotmail.com>
2024-11-12 15:37:59 +0300

    pyramid solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,18 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    const width = 2 * n - 1;
+
+    for(let row=0; row<n; row++) {
+        let level = '';
+        const hashes = 2 * row + 1;
+        const spaces = (width-hashes) / 2;
+
+        level += ' '.repeat(spaces) + '#'.repeat(hashes) + ' '.repeat(spaces);
+
+        console.log(level)
+    }
+}
 
 module.exports = pyramid;
